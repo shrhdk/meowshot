3f9551c14a01a6a4a2224cee7c736d699d9f82b4
Hideki SHIRO <hideki@shiro.be>
2018-12-20 18:42:08 +0900

    update theta-plugin-sdk to v0.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 dependencies {
     implementation(kotlin("stdlib-jdk8", KotlinCompilerVersion.VERSION))
     implementation("com.android.support.constraint", "constraint-layout", "1.1.3")
-    implementation("org.theta4j", "theta-plugin-sdk", "0.0.2")
+    implementation("org.theta4j", "theta-plugin-sdk", "0.0.3")
     implementation("org.nanohttpd", "nanohttpd", "2.2.0")
 
     testImplementation("junit", "junit", "4.12")
