9d98b26b3e74492b88913ff1b3a3e2d71259f6a3
Hideki SHIRO <hideki@shiro.be>
2019-05-31 00:13:14 +0900

    Update build tools.

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,8 +20,8 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build", "gradle", "3.3.0")
-        classpath(kotlin("gradle-plugin", "1.3.21"))
+        classpath("com.android.tools.build", "gradle", "3.4.1")
+        classpath(kotlin("gradle-plugin", "1.3.31"))
     }
 }
 
